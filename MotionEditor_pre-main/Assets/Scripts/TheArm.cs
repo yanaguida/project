de422352ee4229b7707ac8184562379afacf1505
Fy0929 <b1022192@fun.ac.jp>
2025-07-23 20:02:24 +0900

    Update TheArm.cs

--- MotionEditor_pre-main/Assets/Scripts/TheArm.cs
+++ MotionEditor_pre-main/Assets/Scripts/TheArm.cs
@@ -1,68 +1,78 @@
 using UnityEngine;
+using System.Collections;
 
 public class TheArm : MonoBehaviour
 {
   /*ミラガラの羽を上下に動かすためのスクリプト
-  Right_Arm,Left_ArmはUnity上で動かしたい部位を指定する必要がある。
-  Right_Arm_Angle,Left_Arm_Angle,maxAngle,minAngle←これらで羽の可動域を制限
-  isUpで上げ下げの切り替えを制御
+  コルーチン型の関数RightArmとLeftArm
+  第一引数で上か下か、第二引数で、どこまで上げるか
+  bool型関数StopMotionで無限ループから抜ける
   */
     public Transform Right_Arm;
     public Transform Left_Arm;
     public float Right_Arm_Angle = 0f;
     public float Left_Arm_Angle = 0f;
-    public float maxAngle = 120f;
-    public float rotationSpeed = 30f;
-    public float  minAngle = 0f; 
-    private float elapsedTime = 0f;     // 経過時間
-    private bool isUP=true;
+    private float rotationSpeed = 30f;
+    private float subtle=1.5f;
+    private bool isRightArmMoving = false;
+    private bool isLeftArmMoving = false;
 
-public void Arm()
-    {
+    public IEnumerator RightArm(bool isUP,float desiredAngle){
+        if (isRightArmMoving){
+            yield break;
+        }
+        isRightArmMoving = true;
+        while(true){
+            if (!StopMotionR(desiredAngle))
+                break;
         float deltaAngle = rotationSpeed * Time.deltaTime;
-        //現在の角度と入力された角度を比較、上下方向を設定
-        if(Control.instance.Getlean(1)=="1"){
-        if(float.Parse(Control.instance.GetValues(1))>Right_Arm_Angle)
-        isUP=true;
-        else if(float.Parse(Control.instance.GetValues(1))<Right_Arm_Angle)
-        isUP=false;
+        if (isUP){
+            Right_Arm.Rotate(Vector3.forward * -deltaAngle);//上方向に動かす
+            Right_Arm_Angle += deltaAngle;//アームの可動域の制限
         }
-        if(Control.instance.Getlean(1)=="2"){
-        if(float.Parse(Control.instance.GetValues(1))>Left_Arm_Angle)
-        isUP=true;
-        else if(float.Parse(Control.instance.GetValues(1))<Left_Arm_Angle)
-        isUP=false;
+        else{
+            Right_Arm.Rotate(Vector3.forward * deltaAngle);//下方向に動かす
+            Right_Arm_Angle -= deltaAngle;//アームの可動域の制限
         }
-        //右腕部分
-    if (Control.instance.Getlean(1)=="1"&&isUP==true&&float.Parse(Control.instance.GetValues(1))>Right_Arm_Angle){
-        Right_Arm.Rotate(Vector3.forward * -deltaAngle);//上方向に動かす
-        Right_Arm_Angle += deltaAngle;//アームの可動域の制限
-        if(float.Parse(Control.instance.GetValues(1))==Right_Arm_Angle)
-       return ;
+        yield return null;
+        }
+        isRightArmMoving = false;
     }
-    
-   if (Control.instance.Getlean(1)=="1"&&isUP==false&&float.Parse(Control.instance.GetValues(1))<Right_Arm_Angle){
-        Right_Arm.Rotate(Vector3.forward * deltaAngle);//下方向に動かす
-        Right_Arm_Angle -= deltaAngle;//アームの可動域の制限
-        if(float.Parse(Control.instance.GetValues(1))==Right_Arm_Angle)
-        return ;
-        }
 
-    
-    //左腕部分
-    if (Control.instance.Getlean(1)=="2"&&isUP==true&&float.Parse(Control.instance.GetValues(1))>Left_Arm_Angle){
-        Left_Arm.Rotate(Vector3.up * deltaAngle);//上方向に動かす
-        Left_Arm_Angle += deltaAngle;//アームの可動域の制限
-        if(float.Parse(Control.instance.GetValues(1))==Left_Arm_Angle)
-    return ;
+    public IEnumerator LeftArm(bool isUP,float desiredAngle){
+        if (isLeftArmMoving){
+            yield break;
+        }
+        isLeftArmMoving = true;
+        while(true){
+            if (!StopMotionL(desiredAngle))
+                break;
+        float deltaAngle = rotationSpeed * Time.deltaTime;
+        if (isUP){
+            Left_Arm.Rotate(Vector3.up * deltaAngle);//上方向に動かす
+            Left_Arm_Angle += deltaAngle;//アームの可動域の制限
+        }
+        else{
+            Left_Arm.Rotate(Vector3.up * -deltaAngle);//下方向に動かす
+            Left_Arm_Angle -= deltaAngle;//アームの可動域の制限
+        }
+        yield return null;
+        }
+        isLeftArmMoving = false;
     }
-    
-   if (Control.instance.Getlean(1)=="2"&&isUP==false&&float.Parse(Control.instance.GetValues(1))<Left_Arm_Angle){
-        Left_Arm.Rotate(Vector3.up * -deltaAngle);//下方向に動かす
-        Left_Arm_Angle -= deltaAngle;//アームの可動域の制限
-        if(float.Parse(Control.instance.GetValues(1))==Left_Arm_Angle)
-    return ;
-    
+
+    public IEnumerator Empty(float desiredTime){
+        yield return new WaitForSeconds(desiredTime);
     }
-}
+
+    public bool StopMotionR(float desiredAngle){
+        float absValue = Mathf.Abs(Right_Arm_Angle - desiredAngle);
+        return absValue > subtle;
+
+    }
+
+    public bool StopMotionL(float desiredAngle){
+        float absValue = Mathf.Abs(Left_Arm_Angle - desiredAngle);
+        return absValue > subtle;
+    }
 }

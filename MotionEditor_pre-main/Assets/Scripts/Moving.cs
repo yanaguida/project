6ace4f416c838c6d34e4662e0b8301b326584085
Fy0929 <b1022192@fun.ac.jp>
2025-07-23 19:02:30 +0900

    Create Moving.cs

---
+++ MotionEditor_pre-main/Assets/Scripts/Moving.cs
@@ -0,0 +1,23 @@
+﻿using JetBrains.Annotations;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+public class Moving : MonoBehaviour
+{
+    public float WhereScroll;
+    public ScrollRect scrollRect;
+
+
+    //スクロール
+    void Start()
+    {
+    scrollRect = GetComponent<ScrollRect>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //なにもしない
+    }
+
+}

2800cc3339c9d79503dafca90569b020c65ea911
Fy0929 <b1022192@fun.ac.jp>
2025-07-26 17:34:36 +0900

    Update DropDownClick.cs

--- MotionEditor_pre-main/Assets/Scripts/DropDownClick.cs
+++ MotionEditor_pre-main/Assets/Scripts/DropDownClick.cs
@@ -6,20 +6,8 @@ public class DropDownClick : MonoBehaviour, IPointerClickHandler
 
     public int num; //←←←partNによって変更する、オブジェクト側で入力可能
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void OnPointerClick(PointerEventData eventData)
     {
-        Control.instance.SetCPfromClick(num);
+        Control.instance.SetChoiceParts(num);
     }
 }

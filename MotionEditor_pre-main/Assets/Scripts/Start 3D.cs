aea092eb57b8d7953e54c275562fe2c4a09fe8d2
Fy0929 <b1022192@fun.ac.jp>
2025-08-12 14:44:24 +0900

    Update Start 3D.cs

--- MotionEditor_pre-main/Assets/Scripts/Start 3D.cs
+++ MotionEditor_pre-main/Assets/Scripts/Start 3D.cs
@@ -13,8 +13,6 @@ public class Start3D : MonoBehaviour
     public ParticleSystem electricEffect;
     public GameObject StartButton;
     public GameObject StopButton;
-    private bool isUP_R;
-    private bool isUP_L;
 
     public void Start(){
         SwitchText(true);
@@ -34,12 +32,10 @@ public class Start3D : MonoBehaviour
             int index = kvp.Key;
             Control.InputData data = kvp.Value;
             if(data.partNumber == 1||data.partNumber == 11||data.partNumber == 12){
-                SetisUP_R(data.value);
-                yield return StartCoroutine(armScript.RightArm(data.time,data.value,isUP_R));
+                yield return StartCoroutine(armScript.RightArm(data.time,data.value));
             }
             if(data.partNumber == 2||data.partNumber == 21||data.partNumber == 22){
-                SetisUP_L(data.value);
-                yield return StartCoroutine(armScript.LeftArm(data.time,data.value,isUP_L));
+                yield return StartCoroutine(armScript.LeftArm(data.time,data.value));
             }
         }
     }
@@ -47,18 +43,5 @@ public class Start3D : MonoBehaviour
     private void SwitchText(bool i){
         StartButton.SetActive(i);
         StopButton.SetActive(!i);
-    }
-
-    public void SetisUP_R(float DesiredAngle){
-        if(DesiredAngle>armScript.Right_Arm_Angle)
-            isUP_R=true;
-        else
-            isUP_R=false;
-    }
-    public void SetisUP_L(float DesiredAngle){
-        if(DesiredAngle>armScript.Left_Arm_Angle)
-            isUP_L=true;
-        else
-            isUP_L=false;
     }
 }

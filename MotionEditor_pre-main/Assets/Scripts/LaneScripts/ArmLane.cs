e5a5acfb0d39c9d6fa1bd0ab628599cf62005732
Fy0929 <b1022192@fun.ac.jp>
2025-09-24 15:32:36 +0900

    Update ArmLane.cs

--- MotionEditor_pre-main/Assets/Scripts/LaneScripts/ArmLane.cs
+++ MotionEditor_pre-main/Assets/Scripts/LaneScripts/ArmLane.cs
@@ -4,77 +4,26 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 using System.Collections;
 
-public class ArmData
-{
-    public float start;
-    public float time;
-    public float value;
-
-    public ArmData(float start, float time, float value)
-    {
-        if(0<start)
-        this.start = start;
-
-        if(0<time)
-        this.time = time;
-
-        if(0<value&&value<360)
-        this.value = value;
-    }
-}
-
 public enum armKind{Right,Left}
 
-public class ArmLane : Lanes
+public class ArmLane : Lanes<ArmData>
 {
-    private armKind armkind;
-    private List<ArmData> LaneData = new List<ArmData>();
+    public Motors motorScript; 
+    public armKind armkind;
 
-    public void SetKind(armKind kind){
-        this.armkind = kind;
+    protected override ArmData CreateDataFromChild(Transform child){
+        ArmIcon icon = child.GetComponent<ArmIcon>();
+        if(icon!=null)
+        return new ArmData(icon.GetStart(),icon.GetTime(),icon.GetValue());
+        else
+        Debug.Log("ArmIconの取得を失敗");
+        return null;
     }
 
-    public override void SetLaneData(){
-        LaneData.Clear();
-        foreach (Transform child in transform){
-            ArmIcon Arm = child.GetComponent<ArmIcon>();
-            LaneData.Add(new ArmData(Arm.GetStart(),Arm.GetTime(),Arm.GetValue()));
-        }
-        LaneData.Sort((a, b) => a.start.CompareTo(b.start));
+    protected override IEnumerator ExecuteAction(ArmData data){
+        yield return StartCoroutine(motorScript.Motor(data.time,data.value));
     }
 
-    public override IEnumerator ExecuteLane()
-    {
-        float elapsedtime = 0f;
-        for (int i = 0; i < LaneData.Count; i++)
-        {
-            ArmData data = LaneData[i];
-            float correctstart = data.start - elapsedtime;
-            
-            Debug.Log($"Index: {i}, Start: {correctstart}, Time: {data.time},Value: {data.value}");
-
-            yield return StartCoroutine(funScript.Wait(correctstart));
-
-            if(armkind == armKind.Right)
-            yield return StartCoroutine(funScript.RightArm(data.time, data.value));
-
-            else if(armkind == armKind.Left)
-            yield return StartCoroutine(funScript.LeftArm(data.time, data.value));
-
-            elapsedtime += correctstart+data.time;
-        }
-    }
-
-    public override float GetTotalTime(){
-        float adjustedstart = 0f;
-        float elapsedtime = 0f;
-        float totaltime = 0f;
-        for(int i=0; i<LaneData.Count; i++){
-            ArmData data = LaneData[i];
-            adjustedstart = data.start - elapsedtime;
-            elapsedtime += adjustedstart+data.time;
-            totaltime = totaltime+adjustedstart+data.time;
-        }
-        return totaltime;
-    }
+    protected override float GetStart(ArmData data) => data.start;
+    protected override float GetTime(ArmData data) => data.time;
 }

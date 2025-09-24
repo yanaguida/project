7bea3943e61af3f04a70e6c0b48c45c15f5cb591
Fy0929 <b1022192@fun.ac.jp>
2025-09-24 15:32:28 +0900

    Update ArmIcon.cs

--- MotionEditor_pre-main/Assets/Scripts/IconScripts/ArmIcon.cs
+++ MotionEditor_pre-main/Assets/Scripts/IconScripts/ArmIcon.cs
@@ -1,6 +1,29 @@
 using UnityEngine;
 using TMPro;
 
+public class ArmData
+{
+    public float start;
+    public float time;
+    public float value;
+
+    public ArmData(float start, float time, float value)
+    {
+        if(0 <= start)
+        this.start = start;
+        else
+        Debug.Log("start値が不正です");
+        if(0 <= time)
+        this.time = time;
+        else
+        Debug.Log("time値が不正です");
+        if(0<=value&&value<=360)
+        this.value = value;
+        else
+        Debug.Log("value値が不正です");
+    }
+}
+
 public class ArmIcon : Icons
 {
     private float value;
@@ -21,72 +44,4 @@ public class ArmIcon : Icons
             value = x;
         }
     }
-/*
-    protected override void SetUI(RectTransform laneRect, Vector2 dropPos){
-        IconRect.SetParent(laneRect, false);
-        Vector2 localPos;
-        RectTransformUtility.ScreenPointToLocalPointInRectangle(
-            laneRect,
-            dropPos,
-            Camera.main,
-            out localPos
-        );
-        localPos.y = 0f;
-        localPos.x = Mathf.Round(localPos.x / snapInterval) * snapInterval+dtime;
-        SetStart(localPos.x);
-        IconRect.anchoredPosition = localPos;
-    }
-
-    protected override void SetStart(float x){
-        start = (x+dtime-20f+adjustX)*distanceRate;
-    }
-
-
-    public void Extend(){
-        if(CheckRightSideDraggables()){
-            Vector2 size = IconRect.sizeDelta;
-            Vector2 pos = IconRect.anchoredPosition;
-            size.x += 100;
-            pos.x += 50; 
-            IconRect.sizeDelta = size;
-            IconRect.anchoredPosition = pos;
-            time += step;
-            dtime -= 50f;
-        }
-    }
-
-    public void Shrink(){
-        Vector2 size = IconRect.sizeDelta;
-        Vector2 pos = IconRect.anchoredPosition;
-        if (size.x > 100) {
-            size.x -= 100;
-            pos.x -= 50;
-            IconRect.sizeDelta = size;
-            IconRect.anchoredPosition = pos;
-            time -= step;
-            dtime += 50f;
-        }
-    }
-
-    private bool CheckRightSideDraggables()
-    {
-        GameObject[] draggableObjects = GameObject.FindGameObjectsWithTag("Draggable");
-
-        foreach (GameObject targetobj in draggableObjects)
-        {
-            if (targetobj == this.gameObject) continue; 
-            float rightend = start+time;
-            float leftend = 0f;
-            Icons targeticon = targetobj.GetComponent<Icons>();
-            if (targeticon != null){
-                leftend = targeticon.GetStart();
-            }
-            float dx = leftend - rightend;
-            if (dx==0)
-            {
-                return false;
-            }
-        }
-        return true;
-    }*/
 }

064fbeed1e0d15fb672a6a8ce57e3ac4d752f4f9
Fy0929 <b1022192@fun.ac.jp>
2025-09-24 15:32:48 +0900

    Update SelectLane.cs

--- MotionEditor_pre-main/Assets/Scripts/LaneScripts/SelectLane.cs
+++ MotionEditor_pre-main/Assets/Scripts/LaneScripts/SelectLane.cs
@@ -4,63 +4,25 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 using System.Collections;
 
-public class SelectPartData
+public class SelectLane : Lanes<SelectPartData>
 {
-    public float start;
-    public float time;
-    public string emotion;
+    public LED ledScript;
 
-    public SelectPartData(float start, float time, string emotion){
-        if(0<start)
-        this.start = start;
-        if(0<time)
-        this.time = time;
-        this.emotion = emotion;
-    }
-}
-
-public class SelectLane : Lanes
-{
-    public SwitchOn switchScript;
-    private List<SelectPartData> LaneData = new List<SelectPartData>(); 
-
-    public override void SetLaneData(){
-        LaneData.Clear();
-        foreach (Transform child in transform){
-            SelectIcon Icon = child.GetComponent<SelectIcon>();
-            LaneData.Add(new SelectPartData(Icon.GetStart(),Icon.GetTime(),Icon.GetEmotion()));
-        }
-        LaneData.Sort((a, b) => a.start.CompareTo(b.start));
-    }
-
-    public override IEnumerator ExecuteLane()
+    protected override SelectPartData CreateDataFromChild(Transform child)
     {
-        float elapsedtime = 0f;
-        for (int i = 0; i < LaneData.Count; i++)
-        {
-            SelectPartData data = LaneData[i];
-            float correctstart = data.start - elapsedtime;
-            
-            Debug.Log($"Index: {i}, Start: {correctstart}, Time: {data.time},Value: {data.emotion}");
-
-            yield return new WaitForSeconds(correctstart);
-
-            yield return StartCoroutine(switchScript.Toggle(data.time, data.emotion));
-
-            elapsedtime += correctstart+data.time;
-        }
+        SelectIcon icon = child.GetComponent<SelectIcon>();
+        if (icon != null)
+            return new SelectPartData(icon.GetStart(), icon.GetTime(), icon.GetEmotion());
+        else
+        Debug.Log("SelectIconの取得に失敗");
+        return null;
     }
 
-    public override float GetTotalTime(){
-        float adjustedstart = 0f;
-        float elapsedtime = 0f;
-        float totaltime = 0f;
-        for(int i=0; i<LaneData.Count; i++){
-            SelectPartData data = LaneData[i];
-            adjustedstart = data.start - elapsedtime;
-            elapsedtime += adjustedstart+data.time;
-            totaltime = totaltime+adjustedstart+data.time;
-        }
-        return totaltime;
+    protected override IEnumerator ExecuteAction(SelectPartData data)
+    {
+        yield return StartCoroutine(ledScript.Toggle(data.time, data.emotion));
     }
+
+    protected override float GetStart(SelectPartData data) => data.start;
+    protected override float GetTime(SelectPartData data) => data.time;
 }

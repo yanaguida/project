d1e8598ce61b9c2224e801a01c4103be3d7be5fd
Fy0929 <b1022192@fun.ac.jp>
2025-09-24 15:32:32 +0900

    Update SelectIcon.cs

--- MotionEditor_pre-main/Assets/Scripts/IconScripts/SelectIcon.cs
+++ MotionEditor_pre-main/Assets/Scripts/IconScripts/SelectIcon.cs
@@ -1,6 +1,25 @@
 using UnityEngine;
 using TMPro;
 
+public class SelectPartData
+{
+    public float start;
+    public float time;
+    public string emotion;
+
+    public SelectPartData(float start, float time, string emotion){
+        if(0<=start)
+        this.start = start;
+        else
+        Debug.Log("start値が不正です");
+        if(0<time)
+        this.time = time;
+        else
+        Debug.Log("time値が不正です");
+        this.emotion = emotion;
+    }
+}
+
 public class SelectIcon : Icons
 {
     public TMP_Dropdown dropdown;

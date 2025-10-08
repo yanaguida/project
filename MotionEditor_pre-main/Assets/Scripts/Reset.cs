64190719e99a7b6c9883694295cb3009456d9fae
Fy0929 <b1022192@fun.ac.jp>
2025-10-08 15:27:54 +0900

    Create Reset.cs

---
+++ MotionEditor_pre-main/Assets/Scripts/Reset.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Reset : MonoBehaviour
+{
+    public GameObject ResetButton;
+    public GameObject RightArmGo;
+    public GameObject LeftArmGo;
+    private Transform  RightArm;
+    private Transform LeftArm;
+    public Motors RightMoterScript;
+    public Motors LeftMoterScript;
+
+    public void Awake(){
+        RightArm =RightArmGo.GetComponent<Transform>();
+        LeftArm =LeftArmGo.GetComponent<Transform>();
+        
+
+    }
+
+    public void ResetAdachi(){
+        if(RightMoterScript.isMotorMoving ||LeftMoterScript.isMotorMoving){
+            Debug.Log("モーターが動いています。");
+            return; 
+        } 
+        RightMoterScript.CurrentAngle = 0f;
+        LeftMoterScript.CurrentAngle = 0f;
+        RightArm.rotation = RightMoterScript.initialRotation;
+        LeftArm.rotation = LeftMoterScript.initialRotation;
+
+    }
+}

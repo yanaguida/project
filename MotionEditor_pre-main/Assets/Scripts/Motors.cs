83a749792c1d6f46e8488df79489ba4638595cb8
Fy0929 <b1022192@fun.ac.jp>
2025-10-08 15:08:09 +0900

    Update Motors.cs

--- MotionEditor_pre-main/Assets/Scripts/Motors.cs
+++ MotionEditor_pre-main/Assets/Scripts/Motors.cs
@@ -90,35 +90,6 @@ public class Motors : MonoBehaviour
 }
 
 
-   /* public IEnumerator Motor(float desiredTime,float desiredAngle){
-        if (isMotorMoving||desiredAngle>MaxAngle||desiredAngle<MinAngle){
-            yield break;
-        }
-        isMotorMoving = true;
-        float distance = Mathf.Abs(desiredAngle - CurrentAngle);
-        float rotationSpeed = distance/desiredTime;
-        if (distance <= subtle){
-            yield return new WaitForSeconds(desiredTime);
-            isMotorMoving = false;
-            yield break;
-        }
-        while(true){
-            distance = Mathf.Abs(CurrentAngle - desiredAngle);
-            if (distance<=subtle)
-                break;
-            float deltaAngle = rotationSpeed * Time.deltaTime;
-            if (desiredAngle>CurrentAngle){
-                MoveMotor(deltaAngle,xyz,direction);
-                CurrentAngle += deltaAngle;
-            }
-            else{
-                MoveMotor(deltaAngle,xyz,direction*-1);
-                CurrentAngle -= deltaAngle;
-            }
-            yield return null; 
-        }
-        isMotorMoving = false;
-    }*/
 
     private void MoveMotor(float deltaAngle,XYZ xyz,int direction){
         if(direction==1||direction==-1){

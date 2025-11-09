d9fc0bf35b1b03afd06cfe2d6099465dc688526c
Fy0929 <b1022192@fun.ac.jp>
2025-11-09 18:29:18 +0900

    Update Reset.cs

--- MotionEditor_pre-main/Assets/Scripts/Reset.cs
+++ MotionEditor_pre-main/Assets/Scripts/Reset.cs
@@ -1,30 +1,31 @@
 using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
 
 public class Reset : MonoBehaviour
 {
-    public GameObject ResetButton;
-    public GameObject RightArmGo;
-    public GameObject LeftArmGo;
-    private Transform  RightArm;
-    private Transform LeftArm;
+    public Playback playback;
+    public Redline redline;
+    public LED led;
     public Motors RightMoterScript;
     public Motors LeftMoterScript;
 
-    public void Awake(){
-        RightArm =RightArmGo.GetComponent<Transform>();
-        LeftArm =LeftArmGo.GetComponent<Transform>();
+    public void Awake()
+    {
+        if (playback == null) Debug.Log("Restsスクリプトでplaybackがnull");
+        if (redline == null) Debug.Log("Restsスクリプトでredlineがnull");
+        if (led == null) Debug.Log("Restsスクリプトでledがnull");
     }
 
-    public void ResetAdachi(){
-        if(RightMoterScript.isMotorMoving ||LeftMoterScript.isMotorMoving){
-            Debug.Log("モーターが動いています。");
-            return; 
-        } 
-        RightMoterScript.CurrentAngle = 0f;
-        LeftMoterScript.CurrentAngle = 0f;
-        RightArm.rotation = RightMoterScript.initialRotation;
-        LeftArm.rotation = LeftMoterScript.initialRotation;
+    public void ResetAdachi()
+    {
+        if (playback.GetModelState() == ModelState.Restart || playback.GetModelState() == ModelState.Play)
+        {
+            playback.Initialize();
+            redline.ResetRedline();
+            led.TurnOff();
+            RightMoterScript.CurrentAngle = 0f;
+            LeftMoterScript.CurrentAngle = 0f;
+            RightMoterScript.initialize();
+            LeftMoterScript.initialize();
+        }
     }
 }

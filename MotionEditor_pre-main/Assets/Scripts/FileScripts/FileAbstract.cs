b549d736bbdf29a84902812cbb812c1d4fad7f2c
Fy0929 <b1022192@fun.ac.jp>
2025-11-09 18:28:30 +0900

    Update FileAbstract.cs

--- MotionEditor_pre-main/Assets/Scripts/FileScripts/FileAbstract.cs
+++ MotionEditor_pre-main/Assets/Scripts/FileScripts/FileAbstract.cs
@@ -1,21 +1,13 @@
 using UnityEngine;
 using TMPro;
-using System.IO;
 using System.Collections.Generic;
-using System.Linq;
 
 public abstract class FileAbstract : MonoBehaviour
 {
     public GameObject screen;
     public TMP_InputField inputField;
-    protected List<ILane> allLanes = new List<ILane>();
+    protected List<Icons> IconList = new List<Icons>();
     protected string fileName;
-
-    void Awake()
-    {
-        // ILaneを継承しているMonoBehaviourをすべて取得
-        allLanes = FindObjectsOfType<MonoBehaviour>().OfType<ILane>().ToList();
-    }
 
     public virtual void OnClick()
     {

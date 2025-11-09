b871fc9ea21ac8dff1c5171fdca87d1033b30a0d
Fy0929 <b1022192@fun.ac.jp>
2025-11-09 18:28:44 +0900

    Update ArmIcon.cs

--- MotionEditor_pre-main/Assets/Scripts/IconScripts/ArmIcon.cs
+++ MotionEditor_pre-main/Assets/Scripts/IconScripts/ArmIcon.cs
@@ -1,32 +1,8 @@
 using UnityEngine;
 using TMPro;
 
-public class ArmData
-{
-    public float start;
-    public float time;
-    public float value;
-
-    public ArmData(float start, float time, float value)
-    {
-        if(0 <= start)
-        this.start = start;
-        else
-        Debug.Log("start値が不正です");
-        if(0 <= time)
-        this.time = time;
-        else
-        Debug.Log("time値が不正です");
-        if(0<=value&&value<=360)
-        this.value = value;
-        else
-        Debug.Log("value値が不正です");
-    }
-}
-
 public class ArmIcon : Icons
 {
-    private float value;
     public TMP_InputField inputField_Arm;
 
     void Start()
@@ -34,20 +10,12 @@ public class ArmIcon : Icons
         inputField_Arm.onValueChanged.AddListener(SetValue);
     }
 
-    public void SetData(float s,float t,float v){
-        start = s;
-        time = t;
-        value = v;
-    }
-
-    public float GetValue(){
-        return value;
-    }
-
-    public void SetValue(string targettime){
+    private void SetValue(string targettime)
+    {
         if (float.TryParse(targettime, out float x))
         {
             value = x;
+            issaved = false;
         }
     }
 }

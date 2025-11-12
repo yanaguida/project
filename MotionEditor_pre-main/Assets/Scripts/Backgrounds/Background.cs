e08898a6cd177a82c15df30585e34961e652a023
Fy0929 <b1022192@fun.ac.jp>
2025-11-12 17:44:22 +0900

    Update Background.cs

--- MotionEditor_pre-main/Assets/Scripts/Backgrounds/Background.cs
+++ MotionEditor_pre-main/Assets/Scripts/Backgrounds/Background.cs
@@ -1,27 +1,55 @@
 using UnityEngine;
-using System.Collections;
 
 public class Background : MonoBehaviour
 {
-    private RectTransform rectTransform;
     public RectTransform bg_meta;
     public RectTransform bg_emo;
-    private Vector2 meta_pos,emo_pos;
-    private int i=0;
+    public RectTransform bg_eva;
+    public RectTransform bg_mira;
+    public ScrollView scrollview;
+    private Vector2 meta_pos, emo_pos, eva_pos, mira_pos;
+    private int i = 0;
 
-    void Start(){
+    void Start()
+    {
         emo_pos = bg_emo.anchoredPosition;
         meta_pos = bg_meta.anchoredPosition;
+        eva_pos = bg_eva.anchoredPosition;
+        mira_pos = bg_mira.anchoredPosition;
     }
 
-    public void OnClick(){
-        if(i%2==0){
-            bg_meta.anchoredPosition = emo_pos;
+    public void OnClick()
+    {
+        if (i % 4 == 0)
+        {
+            bg_meta.anchoredPosition = mira_pos;
             bg_emo.anchoredPosition = meta_pos;
+            bg_eva.anchoredPosition = emo_pos;
+            bg_mira.anchoredPosition = eva_pos;
+            scrollview.RawImageActivate(false);
         }
-        else{
+        else if (i % 4 == 1)
+        {
+            bg_meta.anchoredPosition = eva_pos;
+            bg_emo.anchoredPosition = mira_pos;
+            bg_eva.anchoredPosition = meta_pos;
+            bg_mira.anchoredPosition = emo_pos;
+        }
+        else if (i % 4 == 2)
+        {
+            bg_meta.anchoredPosition = emo_pos;
+            bg_emo.anchoredPosition = eva_pos;
+            bg_eva.anchoredPosition = mira_pos;
+            bg_mira.anchoredPosition = meta_pos;
+            scrollview.SetMetaImage();
+        }
+        else if (i % 4 == 3)
+        {
             bg_meta.anchoredPosition = meta_pos;
             bg_emo.anchoredPosition = emo_pos;
+            bg_eva.anchoredPosition = eva_pos;
+            bg_mira.anchoredPosition = mira_pos;
+            scrollview.RawImageActivate(true);
         }
         i++;
     }
